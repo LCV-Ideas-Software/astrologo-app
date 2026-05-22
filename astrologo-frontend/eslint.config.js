5148e9e03de03560072ce44034f89312cbfee535
LCV-Ideas-Software <lcv@lcv.dev>
2026-05-22 12:02:21 -0300

    chore: align eslint with prettier

--- astrologo-frontend/eslint.config.js
+++ astrologo-frontend/eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -20,4 +21,5 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  eslintConfigPrettier,
 ]);
